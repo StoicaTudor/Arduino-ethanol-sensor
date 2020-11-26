5a62d09b065e6f8676e383153f6ed7ee0ee72484
CSavu <66332005+CSavu@users.noreply.github.com>
2020-11-26 20:36:32 +0200

    Update mq3.php

--- mq3/mq3.php
+++ mq3/mq3.php
@@ -1,17 +1,17 @@
 <?php
 class mq3{
  public $link='';
- function __construct($value){
+ function __construct($value){ // value is the variable to be sent
   $this->connect();
   $this->storeInDB($value);
  }
 
- function connect(){
+ function connect(){              // url and user of the database, no password
   $this->link = mysqli_connect('localhost','root','') or die('Cannot connect to the DB');
   mysqli_select_db($this->link,'alcohol') or die('Cannot select the DB');
- }
+ }                             // send data to the "alcohol" DB
 
- function storeInDB($value){
+ function storeInDB($value){  // the "readings" table inside the "alcohol" DB
   $query = "insert into readings set value='".$value."'";
   $result = mysqli_query($this->link,$query) or die('Errant query:  '.$query);
  }
